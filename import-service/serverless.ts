80f81414f454c86cb6ba332ce3c0cac5c7a88bdb
Aleh Harnizonau <aleh.harnizonau@sap.com>
2023-05-04 15:17:54 +0300

    chore: add cors configuration

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -63,6 +63,23 @@ const serverlessConfiguration: AWS = {
       concurrency: 10,
     },
   },
+  resources: {
+    Resources: {
+      Unauthorized: {
+        Type: 'AWS::ApiGateway::GatewayResponse',
+        Properties: {
+          ResponseParameters: {
+            'gatewayresponse.header.Access-Control-Allow-Origin': "'*'",
+            'gatewayresponse.header.Access-Control-Allow-Headers': "'*'",
+          },
+          ResponseType: 'DEFAULT_4XX',
+          RestApiId: {
+            Ref: 'ApiGatewayRestApi'
+          },
+        },
+      },
+    },
+  },
 };
 
 module.exports = serverlessConfiguration;
